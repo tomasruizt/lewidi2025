cb3e5f26f4070e8f3c1f9972f243c9263be433fd
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-05-20 11:13:29 +0200

    Run multiple loops

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Start the vLLM server
 In thinking mode:
 
 ```shell
-vllm serve Qwen/Qwen3-0.6B \
+vllm serve Qwen/Qwen3-1.7B \
     --dtype auto \
     --enable-reasoning \
     --reasoning-parser deepseek_r1 \
@@ -18,7 +18,7 @@ vllm serve Qwen/Qwen3-0.6B \
 Use non-thinking mode, as describe in the [Qwen3 docs](https://qwen.readthedocs.io/en/latest/deployment/vllm.html#thinking-non-thinking-modes):
 
 ```shell
-vllm serve Qwen/Qwen3-0.6B \
+vllm serve Qwen/Qwen3-1.7B \
     --dtype auto \
     --chat-template ./qwen3_nonthinking.jinja
     --task generate \
--- inference.py
+++ inference.py
@@ -4,12 +4,10 @@ import uuid
 from llmlib.vllm_model import ModelvLLM
 from llmlib.base_llm import Message
 import logging
-from pydantic import Field
-from tqdm import tqdm
+import pandas as pd
+from tqdm import tqdm, trange
 from lewidi_lib.src.funcs import load_dataset, enable_logging
 from pydantic_settings import BaseSettings
-
-enable_logging()
 
 logger = logging.getLogger(__name__)
 
@@ -18,22 +16,11 @@ class Args(BaseSettings, cli_parse_args=True):
     model_id: str = "Qwen/Qwen3-0.6B"
     temperature: float = 0.7
     dataset_name: str = "CSC"
-    run_id: uuid.UUID = Field(default_factory=uuid.uuid4)
     n_examples: int = 10
     max_tokens: int = 5000
     remote_call_concurrency: int = 16
-    run_start: datetime.datetime = Field(default_factory=datetime.datetime.now)
+    n_loops: int = 1
 
-
-args = Args()
-
-df = load_dataset(dataset_name=args.dataset_name)
-
-model = ModelvLLM(
-    remote_call_concurrency=args.remote_call_concurrency,
-    model_id=args.model_id,
-    temperature=args.temperature,
-)
 
 template = """
 Your task is to estimate the distribution of labels that annotators will assign in the task.
@@ -54,16 +41,38 @@ Try to be concise in your reasoning.
 {text}
 """
 
-fixed_data = args.model_dump()
 
-prompts = (template.format(text=t) for t in df.head(args.n_examples)["text"])
-batchof_convos = ([Message.from_prompt(p)] for p in prompts)
-responses = model.complete_batch(batch=batchof_convos, max_tokens=args.max_tokens)
-tgt_file = "responses.jsonl"
-for response in tqdm(responses, total=args.n_examples):
-    data = fixed_data | response
-    data["timestamp"] = datetime.datetime.now().isoformat()
+def run_inference(
+    args: Args, df: pd.DataFrame, model: ModelvLLM, template: str, tgt_file: str
+):
+    fixed_data = args.model_dump()
+    fixed_data["run_id"] = uuid.uuid4()
+    fixed_data["run_start"] = datetime.datetime.now().isoformat()
 
-    with open(tgt_file, "a") as f:
-        json_str = json.dumps(data, default=str)
-        f.write(json_str + "\n")
+    prompts = (template.format(text=t) for t in df.head(args.n_examples)["text"])
+    batchof_convos = ([Message.from_prompt(p)] for p in prompts)
+    responses = model.complete_batch(batch=batchof_convos, max_tokens=args.max_tokens)
+    for response in tqdm(responses, total=args.n_examples):
+        data = fixed_data | response
+        data["timestamp"] = datetime.datetime.now().isoformat()
+
+        with open(tgt_file, "a") as f:
+            json_str = json.dumps(data, default=str)
+            f.write(json_str + "\n")
+
+
+if __name__ == "__main__":
+    enable_logging()
+    args = Args()
+
+    df = load_dataset(dataset_name=args.dataset_name)
+
+    model = ModelvLLM(
+        remote_call_concurrency=args.remote_call_concurrency,
+        model_id=args.model_id,
+        temperature=args.temperature,
+    )
+
+    tgt_file = "responses.jsonl"
+    for _ in trange(args.n_loops):
+        run_inference(args, df, model, template, tgt_file)
