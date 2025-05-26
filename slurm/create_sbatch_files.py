dd5c171391496cde9096566b8f36e02e3a9cfdce
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-05-26 18:56:21 +0200

    Include split in template.sbatch

--- slurm/create_sbatch_files.py
+++ slurm/create_sbatch_files.py
@@ -12,14 +12,16 @@ MODELS = [
 ]
 DATASETS = ["MP", "CSC"]
 GEN_KWARGS = ["thinking", "nonthinking"]
+SPLITS = ["train", "dev"]
 BASE_PORT = 9000
 
 
 os.makedirs("slurm_scripts", exist_ok=True)
 
-for i, (model, dataset, gen_kwargs) in enumerate(product(MODELS, DATASETS, GEN_KWARGS)):
+combinations = product(MODELS, DATASETS, GEN_KWARGS, SPLITS)
+for i, (model, dataset, gen_kwargs, split) in enumerate(combinations):
     port = BASE_PORT + i
-    jobname = f"{model.replace('/', '_')}_{dataset}_{gen_kwargs}"
+    jobname = f"{model.replace('/', '_')}_{dataset}_{split}_{gen_kwargs}"
     template: str = Path("template.sbatch").read_text()
     filled = template.format(
         JOB_NAME=jobname,
@@ -27,6 +29,7 @@ for i, (model, dataset, gen_kwargs) in enumerate(product(MODELS, DATASETS, GEN_K
         GEN_KWARGS=gen_kwargs,
         DATASET=dataset,
         VLLM_PORT=port,
+        SPLIT=split,
     )
     script_path = Path(f"slurm_scripts/{jobname}.sbatch")
     script_path.write_text(filled)
