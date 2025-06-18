acc914ae58db15d65c3d0e5c673f845a3ebadccc
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-06-18 15:54:43 +0200

    Add code checking that Gemini BoN sampling helps in MATH problems

---
+++ lewidi_lib/src/prm800k.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+from typing import Literal
+import numpy as np
+import pandas as pd
+import os
+
+Split = Literal["test", "train"]
+
+
+def load_prm800k_phase2_dataset(split: Split) -> pd.DataFrame:
+    prm800k_df = load_raw_prm800k_phase2_dataset(split)
+
+    new_rows = []
+
+    for _, row in prm800k_df.iterrows():
+        finish_reason = row["label"]["finish_reason"]
+        if finish_reason == "give_up":
+            continue
+
+        completions = [step["completions"][0] for step in row["label"]["steps"]]
+        tgt = row["question"]["ground_truth_answer"]
+        pred = row["question"]["pre_generated_answer"]
+        ratings = np.array([c["rating"] for c in completions])
+        texts = [c["text"] for c in completions]
+        assert len(texts) == len(ratings)
+        new_row = {
+            "dataset_idx": row["dataset_idx"],
+            "problem": row["question"]["problem"],
+            "texts": texts,
+            "ratings": ratings,
+            "avg_rating": np.mean(ratings),
+            "any_negative_rating": np.any(ratings < 0),
+            "n_ratings": len(ratings),
+            "target": tgt,
+            "pred": pred,
+            "correct": finish_reason == "solution",
+            "solution": row["question"]["ground_truth_solution"],
+        }
+        new_rows.append(new_row)
+
+    dataset = pd.DataFrame(new_rows)
+
+    uproblem = dataset["problem"].unique()
+    prob_id_df = pd.DataFrame({"problem_id": range(len(uproblem)), "problem": uproblem})
+
+    dataset = prob_id_df.merge(dataset, on="problem")
+    return dataset
+
+
+def load_raw_prm800k_phase2_dataset(split):
+    file = prm800k_file(split)
+    assert file.exists()
+    prm800k_df = pd.read_json(file, lines=True)
+    prm800k_df["dataset_idx"] = range(len(prm800k_df))
+    return prm800k_df
+
+
+def prm800k_file(split: Split):
+    file = (
+        Path(os.environ["DSS_HOME"])
+        / "lewidi-data"
+        / "PRM800K"
+        / f"phase2_{split}.jsonl"
+    )
+    return file
+
+
+def problems_with_50pct_correct_solutions(
+    dataset: pd.DataFrame, n_problem_ids: int
+) -> pd.DataFrame:
+    half_correct = (
+        dataset.groupby("problem_id", as_index=False)
+        .agg(avg_correct=("correct", "mean"), count=("correct", "count"))
+        .query("avg_correct == 0.5")
+        .head(n_problem_ids)
+    )
+    return dataset.query("problem_id in @half_correct.problem_id")
---
+++ prm800k/rate_prm800k.py
@@ -0,0 +1,33 @@
+import json
+from lewidi_lib import dump_response, postprocess_response
+from prm800k import (
+    load_prm800k_phase2_dataset,
+    problems_with_50pct_correct_solutions,
+)
+from prompt_templates import templates_root
+from llmlib.gemini.gemini_code import GeminiAPI
+from llmlib.base_llm import Message, LlmReq
+from tqdm import tqdm
+
+tgt_file = "gemini-prm800k-ratings.jsonl"
+
+dataset = load_prm800k_phase2_dataset(split="test")
+dataset = problems_with_50pct_correct_solutions(dataset, n_problem_ids=10)
+
+template = templates_root / "rate_prm800k.txt"
+batch = []
+for _, row in dataset.iterrows():
+    steps = [{"step": t} for t in row["texts"]]
+    prompt = template.read_text().format(STEPS=json.dumps(steps, indent=2))
+    req = LlmReq(
+        convo=[Message.from_prompt(prompt)],
+        metadata={"dataset_idx": row["dataset_idx"]},
+        gen_kwargs={"max_output_tokens": 10_000},
+    )
+    batch.append(req)
+
+model = GeminiAPI(model_id="gemini-2.5-pro")
+gen = model.complete_batchof_reqs(batch=batch)
+for response in tqdm(gen, total=len(batch)):
+    response = postprocess_response(response)
+    dump_response(response, tgt_file)
