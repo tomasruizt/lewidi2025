493d4676445e5b5f59f1aabbc76ab1890c429a4c
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-06-20 17:00:25 +0200

    Update sbatch templates and creators

--- slurm/judge/create_sbatch_files.py
+++ slurm/judge/create_sbatch_files.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-preds_dirname = "1_cscfull_t31_Qwen_Qwen3-8B_set2"
+
+template_vars = {
+    "PREDS_DIRNAME": "tasks_0_cscfull_t31_Qwen_Qwen3-32B_set2",
+    "JUDGE_MODEL_ID": "Qwen/Qwen3-32B",
+}
 
 template = Path("template.sbatch").read_text()
-filled = template.format(PREDS_DIRNAME=preds_dirname)
+filled = template.format(**template_vars)
 
 tgt_file = Path("slurm_scripts") / "judge.sbatch"
 tgt_file.parent.mkdir(parents=True, exist_ok=True)
--- slurm/preds/create_sbatch_files.py
+++ slurm/preds/create_sbatch_files.py
@@ -22,20 +22,20 @@ def parse_args():
 args = parse_args()
 
 CASES = [
-    # Case("Qwen/Qwen3-0.6B", n_gpus=1),
+    # Case("Qwen/Qwen3-0.6B", n_gpus=1, remote_call_concurrency=128),
     # Case("Qwen/Qwen3-1.7B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-4B", n_gpus=1),
+    # Case("Qwen/Qwen3-4B", n_gpus=1, remote_call_concurrency=128),
     # Case("Qwen/Qwen3-8B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-14B", n_gpus=1),
-    # Case("Qwen/Qwen3-32B", n_gpus=2),
-    Case(
-        "Qwen/Qwen3-235B-A22B",
-        n_gpus=8,
-        remote_call_concurrency=16,
-        enable_expert_parallel=True,
-    ),
+    # Case("Qwen/Qwen3-14B", n_gpus=1, remote_call_concurrency=64),
+    Case("Qwen/Qwen3-32B", n_gpus=2, remote_call_concurrency=32),
+    # Case(
+    #     "Qwen/Qwen3-235B-A22B",
+    #     n_gpus=8,
+    #     remote_call_concurrency=16,
+    #     enable_expert_parallel=True,
+    # ),
 ]
-DATASETS = ["CSC"]
+DATASETS = ["Paraphrase", "VariErrNLI"]
 GEN_KWARGS = ["set2"]  # , "set1"]
 SPLITS = ["train"]  # "dev"]
 TEMPLATE_IDS = ["31"]  # ["0", "1", "2", "3", "4"]
@@ -52,7 +52,7 @@ combinations = product(CASES, GEN_KWARGS)
 for i, (case, gen_kwargs) in enumerate(combinations):
     # Base port for this combination - each array task will add its task ID to this
     port = BASE_PORT + (i * 100)  # Give enough space between job ports
-    jobname = f"{i}_cscfull_t31_{case.model.replace('/', '_')}_{gen_kwargs}"
+    jobname = f"{i}_Paraphrase_and_VariErrNLI_100_t31_{case.model.replace('/', '_')}_{gen_kwargs}"
     template: str = Path("template.sbatch").read_text()
     filled = template.format(
         JOB_NAME=jobname,
