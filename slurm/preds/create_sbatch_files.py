5f5fd3be574b101dc5eb5e4d52bd5eb565cbd10c
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-07-18 10:17:42 +0200

    Update preds slurm creation script

--- slurm/preds/create_sbatch_files.py
+++ slurm/preds/create_sbatch_files.py
@@ -13,6 +13,12 @@ class Case:
     enable_expert_parallel: bool = False
 
 
+@dataclass
+class DatasetCase:
+    name: str
+    slurm_array_size: int
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--launch", action="store_true")
@@ -22,11 +28,11 @@ def parse_args():
 args = parse_args()
 
 CASES = [
-    # Case("Qwen/Qwen3-0.6B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-1.7B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-4B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-8B", n_gpus=1, remote_call_concurrency=128),
-    # Case("Qwen/Qwen3-14B", n_gpus=1, remote_call_concurrency=64),
+    Case("Qwen/Qwen3-0.6B", n_gpus=1, remote_call_concurrency=128),
+    Case("Qwen/Qwen3-1.7B", n_gpus=1, remote_call_concurrency=128),
+    Case("Qwen/Qwen3-4B", n_gpus=1, remote_call_concurrency=128),
+    Case("Qwen/Qwen3-8B", n_gpus=1, remote_call_concurrency=128),
+    Case("Qwen/Qwen3-14B", n_gpus=1, remote_call_concurrency=64),
     Case("Qwen/Qwen3-32B", n_gpus=2, remote_call_concurrency=32),
     # Case(
     #     "Qwen/Qwen3-235B-A22B",
@@ -35,12 +41,17 @@ CASES = [
     #     enable_expert_parallel=True,
     # ),
 ]
-DATASETS = ["CSC", "MP", "Paraphrase", "VariErrNLI"]
+DATASETS = [
+    DatasetCase("CSC", slurm_array_size=2),
+    DatasetCase("VariErrNLI", slurm_array_size=1),
+    DatasetCase("Paraphrase", slurm_array_size=1),
+    DatasetCase("MP", slurm_array_size=4),
+]
+
 gen_kwargs = "set2"
 split = "train"
-template_id = 33
+template_id = 31
 BASE_PORT = 9000
-slurm_array_size = 4
 
 tgt_dir = Path("slurm_scripts")
 os.makedirs(tgt_dir, exist_ok=True)
@@ -53,10 +64,10 @@ combinations = product(CASES, DATASETS)
 for i, (case, dataset) in enumerate(combinations):
     # Base port for this combination - each array task will add its task ID to this
     port = BASE_PORT + (i * 100)  # Give enough space between job ports
-    jobname = f"t{template_id}_{dataset}_{case.model.replace('/', '_')}"
+    jobname = f"{dataset.name}_{split}_{case.model.replace('/', '_')}_t{template_id}"
     template: str = Path("template.sbatch").read_text()
     tgt_dir = Path(
-        f"/dss/dssfs02/lwp-dss-0001/pn76je/pn76je-dss-0000/lewidi-data/sbatch/di38bec/{case.model.replace('/', '_')}/{gen_kwargs}/t{template_id}/{dataset}/{split}/allex_10loops/preds"
+        f"/dss/dssfs02/lwp-dss-0001/pn76je/pn76je-dss-0000/lewidi-data/sbatch/di38bec/{case.model.replace('/', '_')}/{gen_kwargs}/t{template_id}/{dataset.name}/{split}/allex_10loops/preds"
     )
 
     filled = template.format(
@@ -65,14 +76,14 @@ for i, (case, dataset) in enumerate(combinations):
         JOBNAME=jobname,
         MODEL_ID=case.model,
         GEN_KWARGS=gen_kwargs,
-        DATASETS=dataset,
+        DATASETS=dataset.name,
         SPLITS=split,
         TEMPLATE_IDS=template_id,
         VLLM_PORT=port,
         N_GPUS=case.n_gpus,
         REMOTE_CALL_CONCURRENCY=case.remote_call_concurrency,
         ENABLE_EXPERT_PARALLEL=case.enable_expert_parallel,
-        SLURM_ARRAY_SIZE=slurm_array_size - 1,
+        SLURM_ARRAY_SIZE=dataset.slurm_array_size,
     )
     script_path = Path(f"slurm_scripts/{jobname}.sbatch")
     script_path.write_text(filled)
