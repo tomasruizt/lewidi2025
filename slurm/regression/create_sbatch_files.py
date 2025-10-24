f2d0e5361d035c1d018c10591db0546fe98e4d73
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-10-24 11:01:03 +0200

    Simplify code to create submissions

--- slurm/regression/create_sbatch_files.py
+++ slurm/regression/create_sbatch_files.py
@@ -32,12 +32,56 @@ def partition(n_gpus: int) -> str:
     return string
 
 
+def fill_template(
+    case: Case,
+    dataset: DatasetCase,
+    do_train: bool,
+    tgt_dir: Path,
+    model_dir: Path,
+    full_eval_split: str,
+    jobname: str,
+) -> str:
+    filled = template.format(
+        PARTITION=partition(case.n_gpus),
+        TIME=case.time,
+        N_GPUS=case.n_gpus,
+        SLURM_ARRAY_SIZE=0,
+        LOGS_DIR=tgt_dir / "logs",
+        JOBNAME=jobname,
+        MODEL_ID=case.model,
+        DATASETS=dataset.name,
+        DO_TRAIN=do_train,
+        FULL_EVAL_SPLIT=full_eval_split,
+        PREDS_FILE=tgt_dir / "preds.parquet",
+        TRAIN_INCLUDE_NO_PERSONA=False,
+        SAVED_MODELS_DIR=model_dir / "saved_models",
+    )
+    return filled
+
+
+lewidi_data_root = "/dss/dssfs02/lwp-dss-0001/pn76je/pn76je-dss-0000/lewidi-data"
+
+
+def training_folders(case: Case, dataset: DatasetCase) -> tuple[Path, Path]:
+    tgt_dir = Path(
+        f"{lewidi_data_root}/rlm/{case.model.replace('/', '_')}/{dataset.name}/"
+    )
+    model_dir = Path(str(tgt_dir).replace("rlm", "rlm_models"))
+    return tgt_dir, model_dir
+
+
+def submission_folders(case: Case, dataset: DatasetCase) -> tuple[Path, Path]:
+    tgt_dir, model_dir = training_folders(case, dataset)
+    tgt_dir = Path(str(tgt_dir).replace("rlm", "rlm-submission"))
+    return tgt_dir, model_dir
+
+
 args = parse_args()
 
 CASES = [
     Case("google/t5gemma-s-s-prefixlm", n_gpus=1, time="1:00:00"),
     Case("google/t5gemma-2b-2b-prefixlm", n_gpus=1, time="4:00:00"),
-    Case("google/t5gemma-9b-2b-prefixlm", n_gpus=1, time="12:00:00"),
+    best_known_case := Case("google/t5gemma-9b-2b-prefixlm", n_gpus=1, time="12:00:00"),
     Case("google/t5gemma-9b-9b-prefixlm", n_gpus=1, time="12:00:00"),
 ]
 DATASETS = [
@@ -48,41 +92,51 @@ DATASETS = [
     DatasetCase("CSC,MP,Paraphrase"),
 ]
 
-tgt_dir = Path("slurm_scripts")
-os.makedirs(tgt_dir, exist_ok=True)
+
+scripts_dir = Path("slurm_scripts")
+os.makedirs(scripts_dir, exist_ok=True)
 
 # Clear any existing .sbatch files
-for file in tgt_dir.glob("*.sbatch"):
+for file in scripts_dir.glob("*.sbatch"):
     file.unlink()
 
 
 combinations = product(CASES, DATASETS)
-for i, (case, dataset) in enumerate(combinations):
+for case, dataset in combinations:
     jobname = f"rlm_{dataset.name}_{case.model.replace('/', '_')}"
     template: str = Path("template.sbatch").read_text()
-    tgt_dir = Path(
-        f"/dss/dssfs02/lwp-dss-0001/pn76je/pn76je-dss-0000/lewidi-data/rlm/{case.model.replace('/', '_')}/{dataset.name}/"
+    tgt_dir, model_dir = training_folders(case, dataset)
+    filled = fill_template(
+        case=case,
+        dataset=dataset,
+        do_train=True,
+        tgt_dir=tgt_dir,
+        model_dir=model_dir,
+        full_eval_split="dev",
+        jobname=jobname,
     )
-    model_dir = Path(str(tgt_dir).replace("rlm", "rlm_models"))
-
-    full_eval_split = "dev"
-    filled = template.format(
-        PARTITION=partition(case.n_gpus),
-        TIME=case.time,
-        N_GPUS=case.n_gpus,
-        SLURM_ARRAY_SIZE=0,
-        LOGS_DIR=tgt_dir / "logs",
-        JOBNAME=jobname,
-        MODEL_ID=case.model,
-        DATASETS=dataset.name,
-        FULL_EVAL_SPLIT=full_eval_split,
-        PREDS_FILE=tgt_dir / "preds.parquet",
-        TRAIN_INCLUDE_NO_PERSONA=False,
-        SAVED_MODELS_DIR=model_dir / "saved_models",
-    )
-    script_path = Path(f"slurm_scripts/{jobname}.sbatch")
+    script_path = scripts_dir / f"{jobname}.sbatch"
+    script_path.parent.mkdir(parents=True, exist_ok=True)
     script_path.write_text(filled)
     print(f"Created sbatch file: '{script_path}'")
 
     if args.launch:
         os.system(f"sbatch {script_path}")
+
+# Submission
+for dataset in DATASETS:
+    jobname = f"rlm_submission_{dataset.name}"
+    tgt_dir, model_dir = submission_folders(best_known_case, dataset)
+    filled = fill_template(
+        case=best_known_case,
+        dataset=dataset,
+        do_train=False,
+        tgt_dir=tgt_dir,
+        model_dir=model_dir,
+        full_eval_split="test_clear",
+        jobname=jobname,
+    )
+    script_path = scripts_dir / f"{jobname}.sbatch"
+    script_path.parent.mkdir(parents=True, exist_ok=True)
+    script_path.write_text(filled)
+    print(f"Created sbatch file: '{script_path}'")
